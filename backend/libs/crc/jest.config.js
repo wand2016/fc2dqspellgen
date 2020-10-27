c9bb6257bc1bb624b903c0dda66cf9d49d8d6179
wand2016 <d19921207@gmail.com>
2020-10-27 15:27:59 +0900

    jestのpath mapping

--- backend/libs/crc/jest.config.js
+++ backend/libs/crc/jest.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   roots: ["<rootDir>/src"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
   testMatch: [
     "**/__tests__/**/*.+(ts|tsx|js)",
     "**/?(*.)+(spec|test).+(ts|tsx|js)",
